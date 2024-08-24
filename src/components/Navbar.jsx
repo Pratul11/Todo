ba47a3e03c4e569c389f80cc0fe56017b5daed7f
Pratul Johari <pratuljohari99@gmail.com>
2024-08-24 14:44:47 +0530

    basic working

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <nav>
+            <div className="logo">
+                <p>iTask</p>
+            </div>
+        <ul>
+            <li>HOME</li>
+            <li>ABOUT</li>
+            <li>CONTACT US</li>
+        </ul>
+      
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
